8f33d0d52014fcee89e60dd3ff1193189cbedc06
Tomohito Ozaki <ozaki@yuroyoro.com>
2016-11-16 17:47:56 +0900

    improve examples

--- example_test.go
+++ example_test.go
@@ -16,11 +16,18 @@ func ExampleLaunch() {
 		"/hello",
 		http.StatusOK,
 		"hello, world",
+	).Add(
+		"POST",
+		"/sushi",
+		http.StatusCreated,
+		"🍣",
 	)
 	defer server.Close()
 
 	url := fmt.Sprintf("%s/hello", server.URL)
 	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
 	if err != nil {
 		log.Fatalf("unexpected error : %+v", err)
 	}
@@ -32,10 +39,27 @@ func ExampleLaunch() {
 		log.Fatalf("unexpected error : %+v", err)
 	}
 	fmt.Println(string(body))
+	resp.Body.Close()
+
+	url = fmt.Sprintf("%s/sushi", server.URL)
+	resp, err = http.Post(url, "text/plain", nil)
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+
+	fmt.Println(resp.Status)
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+	fmt.Println(string(body))
 
 	// Output:
 	// 200 OK
 	// hello, world
+	// 201 Created
+	// 🍣
 }
 
 func ExampleLaunch_WithHeader() {
@@ -53,6 +77,8 @@ func ExampleLaunch_WithHeader() {
 
 	url := fmt.Sprintf("%s/hello", server.URL)
 	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
 	if err != nil {
 		log.Fatalf("unexpected error : %+v", err)
 	}
@@ -74,6 +100,72 @@ func ExampleLaunch_WithHeader() {
 	// hello, world
 }
 
+func ExampleLaunch_WithQuery() {
+	server := httpmocker.Launch(
+		httpmocker.Response{
+			Method: "GET",
+			Path:   "/hello",
+			Code:   http.StatusOK,
+			Body:   "hello, world",
+		},
+		httpmocker.Response{
+			Method: "GET",
+			Path:   "/hello",
+			Query:  "dummy=1",
+			Code:   http.StatusOK,
+			Body:   "hello, world with query string",
+		},
+	)
+	defer server.Close()
+
+	// if no query string is given, mock server should return first mock response
+	url := fmt.Sprintf("%s/hello", server.URL)
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+	fmt.Println(string(body))
+	resp.Body.Close()
+
+	// if query string given and matched, mock server should return second mock response
+	url = fmt.Sprintf("%s/hello?dummy=1", server.URL)
+	resp, err = http.Get(url)
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+	fmt.Println(string(body))
+	resp.Body.Close()
+
+	// if query string given and not matched, mock server should return first mock response
+	url = fmt.Sprintf("%s/hello?dummy=2", server.URL)
+	resp, err = http.Get(url)
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("unexpected error : %+v", err)
+	}
+	fmt.Println(string(body))
+
+	// Output:
+	// hello, world
+	// hello, world with query string
+	// hello, world
+}
+
 func ExampleLaunch_WithCustomHandler() {
 	server := httpmocker.Launch(
 		httpmocker.Response{
@@ -89,6 +181,7 @@ func ExampleLaunch_WithCustomHandler() {
 
 	url := fmt.Sprintf("%s/hello", server.URL)
 	resp, err := http.Get(url)
+	defer resp.Body.Close()
 	if err != nil {
 		log.Fatalf("unexpected error : %+v", err)
 	}
@@ -116,6 +209,7 @@ func ExampleLaunch_WithUnknownHandler() {
 
 	url := fmt.Sprintf("%s/sushi", server.URL)
 	resp, err := http.Get(url)
+	defer resp.Body.Close()
 	if err != nil {
 		log.Fatalf("unexpected error : %+v", err)
 	}
